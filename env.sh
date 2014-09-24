dfb21be9d04b0b41eef30b344c052ddb845198d7
Andrey Popov <rainfinder@gmail.com>
2014-09-24 19:36:42 +0200

    Update environment and Makefiles to SL6 and modern versions

--- env.sh
+++ env.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-. /afs/cern.ch/sw/lcg/external/gcc/4.7.1/x86_64-slc5-gcc47-opt/setup.sh
-. /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.01/x86_64-slc5-gcc46-opt/root/bin/thisroot.sh
+source /afs/cern.ch/sw/lcg/external/gcc/4.9.1/x86_64-slc6/setup.sh ""
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.20/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
+
+if [[ -n "$BOOST_ROOT" ]] ; then
+    export BOOST_ROOT="/afs/cern.ch/sw/lcg/external/Boost/1.55.0_python2.7/x86_64-slc6-gcc47-opt"
+    
+    if [ -n $LD_LIBRARY_PATH ] ; then
+        export LD_LIBRARY_PATH=$BOOST_ROOT/lib:$LD_LIBRARY_PATH
+    else
+        export LD_LIBRARY_PATH=$BOOST_ROOT/lib
+    fi
+fi
+
 export PATH=`pwd`/bin:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/cern.ch/sw/lcg/external/Boost/1.50.0_python2.7/x86_64-slc5-gcc46-opt/lib/
